4040d207dcc3cada57de811438788d46102896cd
RsuHyon <72561743+EgorMikhailovich@users.noreply.github.com>
2021-02-25 01:34:06 +0300

    server update

---
+++ config/db.js
@@ -0,0 +1,4 @@
+module.exports = {
+    db  : "mongodb://zedcoin:zedcoin%24123@23.239.24.96:27077/zedcoin?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
+    port : 3000
+}
