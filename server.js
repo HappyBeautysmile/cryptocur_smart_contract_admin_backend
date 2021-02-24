34e3c86e80077cc6bcc3e19f01196092cd7d8de7
DarkBear0121 <darkbear0112@gmail.com>
2021-02-25 01:34:06 +0300

    server update

--- server.js
+++ server.js
@@ -1,31 +1,35 @@
 const express = require("express");
 const app = express();
-const PORT = 3000;
 const mongoose = require("mongoose");
 const routes = require("./routes");
 const logger = require("morgan");
 const flash = require('connect-flash');
 const bodyParser = require('body-parser');
 const cors = require('cors');
+const path = require("path");
+const Dbconfig = require("./config/db");
 
 app.use(cors());
 app.use("*",(req,res,next)=>{
     let expires = new Date(new Date().valueOf() + 30 * 24 * 60 * 60 * 1000);
     res.cookie('cookie1', 'value1', { sameSite: 'lax',httpOnly : true ,expires :expires ,path : "/" });   next(); });
+app.use(express.static("client/build"));
 app.use(bodyParser.json({limit: "15360mb", type:'application/json'}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(logger("dev"));
-app.use(flash())
-app.use(express.static("client/build"));
+app.use(flash());
 app.use("/api", routes);
-app.listen(PORT, (err)=> {
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client/build/index.html'));  
+  });
+app.listen(Dbconfig.port, (err)=> {
     if (err) throw err;
-    console.log(`connected on port ${PORT}`)
+    console.log(`connected on port ${Dbconfig.port}`)
 });
 
-mongoose.connect( "mongodb://localhost/zedcoin", { useNewUrlParser: true ,useFindAndModify: false,useUnifiedTopology: true,useCreateIndex : true }, function(err) {
+mongoose.connect(Dbconfig.db, { useNewUrlParser: true ,useFindAndModify: false,useUnifiedTopology: true,useCreateIndex : true }, function(err) {
     if (err) throw err;
     console.log(`mongoose connection successful`);
 });
