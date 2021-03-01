9330df95edad81b8d2e6553e36437b286586118f
DarkBear0121 <darkbear0112@gmail.com>
2021-03-02 02:45:53 +0300

    update

--- config/db.js
+++ config/db.js
@@ -1,5 +1,5 @@
 module.exports = {
-    db : "mongodb://localhost:27017/zedcoin",
-    // db  : "mongodb://zedcoin:zedcoin%24123@23.239.24.96:27077/zedcoin?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
-    port : 3000
+    // db : "mongodb://localhost:27017/zedcoin",
+    db  : "mongodb://zedcoin:zedcoin%24123@23.239.24.96:27077/zedcoin?serverSelectionTimeoutMS=5000&connectTimeoutMS=10000&authSource=admin&authMechanism=SCRAM-SHA-256",
+    port : 3030
 }
